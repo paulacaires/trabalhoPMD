bdeb7cd09cd6b4d0f61d7d540918db0d915f01bf
Paula Caires <62515804+paulacaires@users.noreply.github.com>
2024-09-02 19:02:01 -0300

    Update MongoSparkConnector.py

--- MongoSparkConnector.py
+++ MongoSparkConnector.py
@@ -9,12 +9,17 @@ spark = SparkSession.\
         config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.13:10.4.0').\
         getOrCreate()
 
-query=(spark.readStream.format("mongodb")
-.option('spark.mongodb.connection.uri', 'mongodb+srv://paulacaires:paula@paula.d7teqh5.mongodb.net/?retryWrites=true&w=majority&appName=Paula')
-    	.option('spark.mongodb.database', 'ReceitasDatabase') \
-    	.option('spark.mongodb.collection', 'Receitas') \
-.option('spark.mongodb.change.stream.publish.full.document.only','true') \
+connection_uri = 'mongodb+srv://paulacaires:paula@paula.d7teqh5.mongodb.net/?retryWrites=true&w=majority&appName=Paula'
+database = "ReceitasDatabase"
+collection = "Receitas"
+
+query = (spark.readStream.format("mongodb").\
+        option('spark.mongodb.connection.uri', connection_uri)
+    	.option('spark.mongodb.database', database) \
+    	.option('spark.mongodb.collection', collection) \
+        .option('spark.mongodb.change.stream.publish.full.document.only','true') \
     	.option("forceDeleteTempCheckpointLocation", "true") \
     	.load())
 
-query.printSchema()
+# Dados para um data frame
+df = spark.read.format("mongodb").option("database", database).option("spark.mongodb.connection.uri", connection_uri).option("collection", collection).load()
